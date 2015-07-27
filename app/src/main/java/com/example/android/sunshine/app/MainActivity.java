4946720dcf6e72a143b2c8d31994e0bb754ff3fa
Michał Stawarz <michal.stawarz@arsthanea.com>
2015-07-27 16:46:45 +0200

    Udacity Sunshine - Lesson 2 Pre-permissions

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.android.sunshine.app;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -23,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -48,44 +54,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> arrayAdapter;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ArrayList<String> arrayList = new ArrayList<String>();
-            arrayList.add("Today - Sunny - 88/63");
-            arrayList.add("Today - Rainy - 88/63");
-            arrayList.add("Today - Whizzly - 88/63");
-            arrayList.add("Today - Whizzly - 88/63");
-            arrayList.add("Today - Whizzly - 88/63");
-            arrayList.add("Tommorrow - Whizzly - 88/63");
-            arrayList.add("Always - Whizzly - 88/63");
-            arrayList.add("Never - Whizzly - 88/63");
-            arrayList.add("Sometimes - Whizzly - 88/63");
-            arrayList.add("Yesterday - Whizzly - 88/63");
-            arrayList.add("Grary - Whizzly - 88/63");
-            arrayList.add("Today1 - Whizzly - 88/63");
-            arrayList.add("Today2 - Whizzly - 88/63");
-            arrayList.add("Today3 - Whizzly - 88/63");
-
-            arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast,R.id.list_item_forecast_textview,arrayList);
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(arrayAdapter);
-
-            return rootView;
-        }
     }
 }
